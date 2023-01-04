6e87070fb5ef7f78fc99c544d98bd0ef7db01b68
Jérôme Jutteau <jerome.jutteau@outscale.com>
2023-01-04 18:43:47 +0100

    Add basic integration tests
    Signed-off-by: Jérôme Jutteau <jerome.jutteau@outscale.com>

--- int-tests/run.sh
+++ int-tests/run.sh
@@ -3,8 +3,9 @@ ROOT=$(cd "$(dirname "${0}")/.." && pwd)
 SHELLCHECK_URL=https://github.com/koalaman/shellcheck/releases/download/v0.8.0/shellcheck-v0.8.0.linux.x86_64.tar.xz
 errors=false
 
-if ! [ -f "$ROOT/target/x86_64-unknown-linux-musl/release/osc-cost" ]; then
-    echo 'error: "target/x86_64-unknown-linux-musl/release/osc-cost" not found'
+oc="$ROOT/target/x86_64-unknown-linux-musl/release/osc-cost"
+if ! [ -f "$oc" ]; then
+    echo "error: \"$oc\" not found"
     errors=true
 fi
 
@@ -36,9 +37,87 @@ if [ "$errors" = true ]; then
     exit 1
 fi
 
-echo -n "self testing with shellcheck... "
-if shellcheck int-tests/run.sh &> /dev/null; then
-    echo "OK"
-else
-    echo "FAILED"
-fi
+function ok_or_die {
+    what="$1"
+    cmd="${*:2}"
+    echo -n "$what ... "
+    if $cmd &> /dev/null ; then
+        echo "OK"
+    else
+        echo "FAILED"
+        exit 1
+    fi
+}
+
+function ko_or_die {
+    what="$1"
+    cmd="${*:2}"
+    echo -n "$what ... "
+    if $cmd &> /dev/null ; then
+        echo "FAILED"
+        exit 1
+    else
+        echo "OK"
+    fi
+}
+
+ok_or_die "self testing with shellcheck" shellcheck int-tests/run.sh
+
+# arg checking
+ok_or_die "help command" "$oc" --help
+ok_or_die "no arg" "$oc"
+ko_or_die "bad arg" "$oc" --bla
+
+# output testing with default input source
+ok_or_die "write json output" "$oc" --format json --output output.json
+ok_or_die "write hour output" "$oc" --format hour --output output.hour
+ok_or_die "write month output" "$oc" --format month --output output.month
+ok_or_die "write csv output" "$oc" --format csv --output output.csv
+
+# format test with api input source
+ok_or_die "json format with implicit api input source" "$oc" --format json
+ok_or_die "hour format with implicit api input source" "$oc" --format hour
+ok_or_die "month format with implicit api input source" "$oc" --format month
+ok_or_die "csv format with implicit api input source" "$oc" --format csv
+
+ok_or_die "json format with explicit api input source" "$oc" --source api --format json
+ok_or_die "hour format with explicit api input source" "$oc" --source api --format hour
+ok_or_die "month format with explicit api input source" "$oc" --source api --format month
+ok_or_die "csv format with explicit api input source" "$oc" --source api --format csv
+
+ok_or_die "aggregate json format with implicit api input source" "$oc" --aggregate --format json
+ko_or_die "aggregate hour format with implicit api input source" "$oc" --aggregate --format hour
+ko_or_die "aggregate month format with implicit api input source" "$oc" --aggregate --format month
+ok_or_die "aggregate csv format with implicit api input source" "$oc" --aggregate --format csv
+
+ok_or_die "aggregate json format with explicit api input source" "$oc" --aggregate --source api --format json
+ko_or_die "aggregate hour format with explicit api input source" "$oc" --aggregate --source api --format hour
+ko_or_die "aggregate month format with explicit api input source" "$oc" --aggregate --source api --format month
+ok_or_die "aggregate csv format with explicit api input source" "$oc" --aggregate --source api --format csv
+
+# format test with json input source
+ok_or_die "json format with implicit json input source" "$oc" --input output.json --format json
+ok_or_die "hour format with implicit json input source" "$oc" --input output.json --format hour
+ok_or_die "month format with implicit json input source" "$oc" --input output.json --format month
+ok_or_die "csv format with implicit json input source" "$oc" --input output.json --format csv
+
+ok_or_die "json format with explicit json input source" "$oc" --input output.json --source json --format json
+ok_or_die "hour format with explicit json input source" "$oc" --input output.json --source json --format hour
+ok_or_die "month format with explicit json input source" "$oc" --input output.json --source json --format month
+ok_or_die "csv format with explicit json input source" "$oc" --input output.json --source json --format csv
+
+ok_or_die "aggregate json format with implicit json input source" "$oc" --aggregate --input output.json --format json
+ko_or_die "aggregate hour format with implicit json input source" "$oc" --aggregate --input output.json --format hour
+ko_or_die "aggregate month format with implicit json input source" "$oc" --aggregate --input output.json --format month
+ok_or_die "aggregate csv format with implicit json input source" "$oc" --aggregate --input output.json --format csv
+
+ok_or_die "aggregate json format with explicit json input source" "$oc" --aggregate --input output.json --source json --format json
+ko_or_die "aggregate hour format with explicit json input source" "$oc" --aggregate --input output.json --source json --format hour
+ko_or_die "aggregate month format with explicit json input source" "$oc" --aggregate --input output.json --source json --format month
+ok_or_die "aggregate csv format with explicit json input source" "$oc" --aggregate --input output.json --source json --format csv
+
+# input should only work with json as input source
+ko_or_die "input as csv should be an error" "$oc" --input output.csv
+ko_or_die "input as hour should be an error" "$oc" --input output.hour
+ko_or_die "input as month should be an error" "$oc" --input output.month
+ko_or_die "--input with --source api" "$oc" --input somefile --source api
