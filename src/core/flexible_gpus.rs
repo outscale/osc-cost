1e9244c83dd87aeeb883c76b6dcc64466156ad63
Maxime Dufour <maxime.dufour@outscale.com>
2023-01-19 13:37:34 +0000

    Add Flexible Gpus

---
+++ src/core/flexible_gpus.rs
@@ -0,0 +1,29 @@
+use serde::{Deserialize, Serialize};
+
+use super::{ResourceError, ResourceTrait, HOURS_PER_MONTH};
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct FlexibleGpu {
+    pub osc_cost_version: Option<String>,
+    pub account_id: Option<String>,
+    pub read_date_rfc3339: Option<String>,
+    pub region: Option<String>,
+    pub resource_id: Option<String>,
+    pub price_per_hour: Option<f32>,
+    pub price_per_month: Option<f32>,
+    pub model_name: Option<String>,
+}
+
+impl ResourceTrait for FlexibleGpu {
+    fn compute(&mut self) -> Result<(), ResourceError> {
+        self.price_per_month = Some(self.price_per_hour.unwrap() * HOURS_PER_MONTH);
+        Ok(())
+    }
+
+    fn price_per_hour(&self) -> Result<f32, ResourceError> {
+        match self.price_per_hour {
+            Some(price) => Ok(price),
+            None => Err(ResourceError::NotComputed),
+        }
+    }
+}
