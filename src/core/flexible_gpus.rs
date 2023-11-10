a9cf77910fefce99471661abd631bcc98514490a
vincent.baer <vincent.baer@outscale.com>
2023-11-10 09:08:01 +0000

    Replace by none emtpy value when it is possible

--- src/core/flexible_gpus.rs
+++ src/core/flexible_gpus.rs
@@ -36,10 +36,10 @@ impl Default for FlexibleGpu {
             account_id: Some("".to_string()),
             read_date_rfc3339: Some("".to_string()),
             region: Some("".to_string()),
-            resource_id: Some("".to_string()),
+            resource_id: None,
             price_per_hour: Some(0.0),
             price_per_month: Some(0.0),
-            model_name: Some("".to_string()),
+            model_name: None,
         }
     }
 }
